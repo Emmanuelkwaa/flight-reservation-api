e0b7c8e8c9978c8f91a241c161dd49c0d1a265a1
ONE BEAST <ONE BEAST@ONE-BEAST>
2022-07-26 14:24:13 -0400

    Initial commit

---
+++ src/main/java/com/skillstorm/flightreservationapi/FlightReservationApiApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.flightreservationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightReservationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightReservationApiApplication.class, args);
+	}
+
+}
