bee9af9852dda2973a97518549d97f94cbe584b6
ONE BEAST <ONE BEAST@ONE-BEAST>
2022-07-27 00:26:47 -0400

    Revert "added constructor, getters and setters, and toString method to models"
    This reverts commit 6bf38efffdd1a5d995753e9f6cbb627a5199115f.

--- src/main/java/com/skillstorm/flightreservationapi/models/City.java
+++ src/main/java/com/skillstorm/flightreservationapi/models/City.java
@@ -5,30 +5,4 @@ public class City {
     private String name;
     private String country;
     private Airport airport;
-
-    public City() {
-    }
-
-    public City(String name, String country, Airport airport) {
-        this.name = name;
-        this.country = country;
-        this.airport = airport;
-    }
-
-    public City(int id, String name, String country, Airport airport) {
-        this.id = id;
-        this.name = name;
-        this.country = country;
-        this.airport = airport;
-    }
-
-    @Override
-    public String toString() {
-        return "City{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", country='" + country + '\'' +
-                ", airport=" + airport +
-                '}';
-    }
 }
