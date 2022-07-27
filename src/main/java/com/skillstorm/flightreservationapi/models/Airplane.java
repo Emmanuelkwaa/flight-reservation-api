e39a56f6c937b0ae5c86f3033a133e82713a9561
ONE BEAST <ONE BEAST@ONE-BEAST>
2022-07-27 00:34:33 -0400

    added constructor, getters and setters, and toString method to model

--- src/main/java/com/skillstorm/flightreservationapi/models/Airplane.java
+++ src/main/java/com/skillstorm/flightreservationapi/models/Airplane.java
@@ -1,9 +1,86 @@
 package com.skillstorm.flightreservationapi.models;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//@Entity
+//@Table
 public class Airplane {
+
+    //@Id
     private String id;
     private Airline airline;
     private int capacity;
     private String model;
     private Seat seat;
+
+    public Airplane() {
+    }
+
+    public Airplane(Airline airline, int capacity, String model, Seat seat) {
+        this.airline = airline;
+        this.capacity = capacity;
+        this.model = model;
+        this.seat = seat;
+    }
+
+    public Airplane(String id, Airline airline, int capacity, String model, Seat seat) {
+        this.id = id;
+        this.airline = airline;
+        this.capacity = capacity;
+        this.model = model;
+        this.seat = seat;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Airline getAirline() {
+        return airline;
+    }
+
+    public void setAirline(Airline airline) {
+        this.airline = airline;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Seat getSeat() {
+        return seat;
+    }
+
+    public void setSeat(Seat seat) {
+        this.seat = seat;
+    }
+
+    @Override
+    public String toString() {
+        return "Airplane{" +
+                "id='" + id + '\'' +
+                ", airline=" + airline +
+                ", capacity=" + capacity +
+                ", model='" + model + '\'' +
+                ", seat=" + seat +
+                '}';
+    }
 }
--- src/main/java/com/skillstorm/flightreservationapi/models/Ticket.java
+++ src/main/java/com/skillstorm/flightreservationapi/models/Ticket.java
@@ -9,4 +9,84 @@ public class Ticket {
     private Date dateOfPurchase;
     private Seat seat;
     private double price;
+
+    public Ticket() {
+    }
+
+    public Ticket(Flight flight, User user, Date dateOfPurchase, Seat seat, double price) {
+        this.flight = flight;
+        this.user = user;
+        this.dateOfPurchase = dateOfPurchase;
+        this.seat = seat;
+        this.price = price;
+    }
+
+    public Ticket(String id, Flight flight, User user, Date dateOfPurchase, Seat seat, double price) {
+        this.id = id;
+        this.flight = flight;
+        this.user = user;
+        this.dateOfPurchase = dateOfPurchase;
+        this.seat = seat;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Flight getFlight() {
+        return flight;
+    }
+
+    public void setFlight(Flight flight) {
+        this.flight = flight;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getDateOfPurchase() {
+        return dateOfPurchase;
+    }
+
+    public void setDateOfPurchase(Date dateOfPurchase) {
+        this.dateOfPurchase = dateOfPurchase;
+    }
+
+    public Seat getSeat() {
+        return seat;
+    }
+
+    public void setSeat(Seat seat) {
+        this.seat = seat;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "id='" + id + '\'' +
+                ", flight=" + flight +
+                ", user=" + user +
+                ", dateOfPurchase=" + dateOfPurchase +
+                ", seat=" + seat +
+                ", price=" + price +
+                '}';
+    }
 }
