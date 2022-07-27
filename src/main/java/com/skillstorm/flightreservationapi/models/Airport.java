5cf0056a4e7dc54f4218e9baed46154340f4b824
ONE BEAST <ONE BEAST@ONE-BEAST>
2022-07-27 14:12:00 -0400

    added annotation for city and airport

--- src/main/java/com/skillstorm/flightreservationapi/models/Airport.java
+++ src/main/java/com/skillstorm/flightreservationapi/models/Airport.java
@@ -1,19 +1,40 @@
 package com.skillstorm.flightreservationapi.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+@Entity
+@Table
 public class Airport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "airport_id")
     private int id;
+
+    @Column
+    @NotBlank
     private String name;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "city_id")
+    private City city;
 
     public Airport() {
     }
 
-    public Airport(String name) {
+    public Airport(String name, City city) {
         this.name = name;
+        this.city = city;
     }
 
-    public Airport(int id, String name) {
+    public Airport(int id, String name, City city) {
         this.id = id;
         this.name = name;
+        this.city = city;
     }
 
     public int getId() {
@@ -32,11 +53,33 @@ public class Airport {
         this.name = name;
     }
 
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Airport airport = (Airport) o;
+        return id == airport.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
     @Override
     public String toString() {
         return "Airport{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", city=" + city +
                 '}';
     }
 }
--- src/main/java/com/skillstorm/flightreservationapi/models/City.java
+++ src/main/java/com/skillstorm/flightreservationapi/models/City.java
@@ -1,25 +1,89 @@
 package com.skillstorm.flightreservationapi.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table
 public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "city_id")
     private int id;
+
+    @Column
+    @NotBlank
     private String name;
+
+    @Column
+    @NotBlank
     private String country;
-    private Airport airport;
+
+    @OneToMany(mappedBy = "airline")
+    private Set<Airport> airport;
 
     public City() {
     }
 
-    public City(String name, String country, Airport airport) {
+    public City(String name, String country, Set<Airport> airport) {
         this.name = name;
         this.country = country;
         this.airport = airport;
     }
 
-    public City(int id, String name, String country, Airport airport) {
+    public City(int id, String name, String country, Set<Airport> airport) {
         this.id = id;
         this.name = name;
         this.country = country;
         this.airport = airport;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Set<Airport> getAirport() {
+        return airport;
+    }
+
+    public void setAirport(Set<Airport> airport) {
+        this.airport = airport;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        City city = (City) o;
+        return id == city.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 
     @Override
